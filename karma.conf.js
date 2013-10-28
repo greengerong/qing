90443b8cb5025b82ff69a45a126f86343bb72bdc
ariesjia <ariesjia00@hotmail.com>
2013-10-28 12:31:59 +0800

    try ci

--- karma.conf.js
+++ karma.conf.js
@@ -42,11 +42,12 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: true
+
   });
 };
