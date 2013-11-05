04c04b4c53a6e7ef03597f6cf854d01e2aa208f3
greengerongg <grzx2210@163.com>
2013-11-05 21:58:28 +0800

    remove app, grunt , ci

--- app/scripts/qing.design.module.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('qing.design', ["qing.product"])
-    .constant('gridConfig', {
-        "totalColumn": 12
-    });
-
--- app/scripts/qing.product.module.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('qing.product', ["pasvaz.bindonce"])
-    .constant('panelConfig', {
-        "url": "scripts/directives/qingPanel/qingPanel.html?mark={0}"
-    });
-
