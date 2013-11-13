9e671cd5e37859c6cc82d96f363dc4f0bb97d685
ariesjia <ariesjia00@hotmail.com>
2013-11-14 01:00:08 +0800

    update

--- TODO.md
+++ TODO.md
@@ -5,8 +5,8 @@
          <del>3 service首字母小写驼峰命名
     <del>2  组件的可编辑，删除
     3  全流程review，设计，代码重构在内
-    4  对qing-add中的plugin罗列使用pluginsService.getAllPlugins返回值根据type=container/control自动生成
-    <del>5  对文本Text可编辑容器的封装 (剩下update，remove 数据保持恢复)
+    <doing>4  对qing-add中的plugin罗列使用pluginsService.getAllPlugins返回值根据type=container/control自动生成
+    5  对文本Text可编辑容器的封装
     6  对基本字符串input with label的封装
     7  对于form类似于row-container的封装，可以设置form的显示方式，Bootstrap form样式罗列。标记修改qing-root-panel增加qing-plugin(设计器指令)
        plugin-data(原始值)。
--- src/design/directives/qingAdd/qingAdd.js
+++ src/design/directives/qingAdd/qingAdd.js
@@ -1,8 +1,19 @@
 'use strict';
 
 angular.module("qing")
-    .directive("qingAdd", ["$compile", "templateService", "pluginModalService",
-        function ($compile, templateService, pluginModalService) {
+    .filter('pluginType',function(){
+        return function(pluginList,pluginType){
+            var plugins = [];
+            angular.forEach(pluginList,function(plugin){
+                if(plugin.type.toLocaleLowerCase() == pluginType){
+                    plugins.push(plugin);
+                }
+            });
+            return plugins;
+        }
+    })
+    .directive("qingAdd", ["$compile", "templateService", "pluginModalService", "$timeout",
+        function ($compile, templateService, pluginModalService, $timeout) {
             return {
                 templateUrl: "design/directives/qingAdd/qingAdd.html",
                 restrict: "EA",
@@ -11,7 +22,6 @@ angular.module("qing")
                 },
                 link: function (scope, element, attrs) {
                     scope.designCallBack = function (pluginName, html) {
-                        //compile on qing-panel scope;
                         $compile(html)(scope.$parent).insertBefore(element);
                         templateService.savePanelTemplate(scope.qingMark, html);
                     };
@@ -23,15 +33,23 @@ angular.module("qing")
                         $scope.addOpen = !$scope.addOpen;
                     };
 
-                    $scope.addCont = function () {
-                        $scope.addOpen = false;
-                    };
+                    // from service .... 暂时还没有注册组件 先测试
+                    $scope.pluginList = [
+                        {
+                            name:'row-container',
+                            title:'Row',
+                            icon:'glyphicon-th',
+                            type:'container'
+                        }
+                    ]
+
+                    $scope.showSubList = function (type){
+                        $scope.subListOpen = $scope.subListOpen == type ? '' : type;
+                    }
 
                     $scope.addContModal = function (pluginName) {
-                        $scope.addOpen = false;
                         pluginModalService.showDesignModal(pluginName)
                             .then(function (result) {
-                                //OK
                                 $scope.designCallBack(pluginName, result);
                             }, function () {
                                 //Cancel
